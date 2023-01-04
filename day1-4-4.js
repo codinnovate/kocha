ebe20063900b316b5dd72bcb0ce69c4078d00112
codinnovate <adeyemis710@gmail.com>
2023-01-04 10:30:00 -0800

    ✨ JavaScript commit on 1/4

---
+++ day1-4-4.js
@@ -0,0 +1,10 @@
+// This function sorts an array of numbers in ascending order using the built-in sort() method.
+function sortNumbersAscending(numbers) {
+  return numbers.sort((a, b) => a - b);
+}
+
+// Example usage:
+const unsortedNumbers = [34, 7, 23, 32, 5, 62];
+const sortedNumbers = sortNumbersAscending(unsortedNumbers);
+
+console.log("Sorted Numbers:", sortedNumbers); // Output: Sorted Numbers: [5, 7, 23, 32, 34, 62]
