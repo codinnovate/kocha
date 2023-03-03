073cb6f6e37831adb535b3a9e58894b8bdb01833
codinnovate <adeyemis710@gmail.com>
2023-03-03 10:30:00 -0800

    ✨ JavaScript commit on 3/3

---
+++ day3-3-0.js
@@ -0,0 +1,11 @@
+// This function takes an array of numbers and returns a new array with the numbers sorted in ascending order.
+function sortNumbers(arr) {
+    return arr.slice().sort((a, b) => a - b);
+}
+
+// Example usage:
+const unsortedNumbers = [5, 3, 8, 1, 2];
+const sortedNumbers = sortNumbers(unsortedNumbers);
+
+console.log('Unsorted:', unsortedNumbers); // Output: Unsorted: [5, 3, 8, 1, 2]
+console.log('Sorted:', sortedNumbers);    // Output: Sorted: [1, 2, 3, 5, 8]
