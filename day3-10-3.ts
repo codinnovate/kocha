5f4cc65dfb7e77709788c819db173889bff3a3a4
codinnovate <adeyemis710@gmail.com>
2023-03-10 18:00:00 -0800

    ✨ TypeScript commit on 3/10

---
+++ day3-10-3.ts
@@ -0,0 +1,11 @@
+// This function takes an array of numbers and returns a new array with the numbers sorted in ascending order.
+function sortNumbers(arr: number[]): number[] {
+    return arr.sort((a, b) => a - b);
+}
+
+// Example usage of the sortNumbers function
+const unsortedNumbers = [34, 7, 23, 32, 5, 62];
+const sortedNumbers = sortNumbers(unsortedNumbers);
+
+console.log("Unsorted Numbers:", unsortedNumbers);
+console.log("Sorted Numbers:", sortedNumbers);
